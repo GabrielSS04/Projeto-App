b4bb92b469326abfc946faa1100d39c4e71a28c1
Gabriel Stainhaus de Souza <gssouzasoftware@gmail.com>
2023-02-15 19:45:03 -0300

    Atualização

--- modificador.js
+++ modificador.js
@@ -1,24 +1,23 @@
+function fnCaptalizar(colecao){
+    var modificado = [];
+
+    for(let i = 0; i < colecao.lenght; i++){
+        var primeiraLetra = colecao[i].charAt(0).toUpperCase();
+        var restoPalavra = colecao[i].slice(1);
+        var captalização = primeiraLetra + restoPalavra;
+        modificado[i] = captalização;
+    }
+
+    return modificado;
+}
+
 function fnOrdenar(vetor){
     return vetor.sort(function(a,b){
         return a.localeCompare(b);
     });
 }
 
-function fnCaptalizar(colecao){
-
-    var modificado = []
-
-    for(let i = 0; i < colecao.lenght; i++){
-        var primeiraLetra = colecao[i].charAt(0).toUpperCase()
-        var restoPalavra = colecao[i].slice(0)
-        var captalização = primeiraLetra + restoPalavra
-        modificado[i] = captalização
-    }
-
-    return modificado
-}
-
-export default{
+export default {
     captalizar: fnCaptalizar,
     ordenar: fnOrdenar
 }
