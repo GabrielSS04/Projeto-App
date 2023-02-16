812ee45a63abac5718ec44aa0c99f32e88cfd486
Gabriel Stainhaus de Souza <gssouzasoftware@gmail.com>
2023-02-15 23:21:14 -0300

    Lista de ingredientes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import modificador from "./modificador.js"
+import ingredientes from "./ingredientes.js"
 
 console.log(modificador)
 
--- ingredientes.js
+++ ingredientes.js
@@ -1,12 +1,56 @@
 var ingredientes = [
 
     {
-        nome: '',
+        nome: 'alface',
         valorAdicional: 0,
-        imagem: 
+        imagem: "Alface.png"
+    },
+    {
+        nome: 'Cebola roxa',
+        valorAdicional: 0,
+        imagem: "Cebola-roxa.png"
+    },
+    {
+        nome: 'Cheddar',
+        valorAdicional: 0,
+        imagem: "Cheddar.png"
+    },
+    {
+        nome: 'Hambúrguer',
+        valorAdicional: 0,
+        imagem: "Hamburguer.png"
+    },
+    {
+        nome: 'Molho de salsa verde',
+        valorAdicional: 0,
+        imagem: "Molho-salsa-verde.png"
+    },
+    {
+        nome: 'Ovo frito',
+        valorAdicional: 0,
+        imagem: "Ovo-frito.png"
+    },
+    {
+        nome: 'Picles',
+        valorAdicional: 0,
+        imagem: "Picles.png"
+    },
+    {
+        nome: 'Queijo coalho',
+        valorAdicional: 0,
+        imagem: "Queijo-coalho.png"
+    },
+    {
+        nome: 'Rúcula',
+        valorAdicional: 0,
+        imagem: "Rucula.png"
+    },
+    {
+        nome: 'Tomate',
+        valorAdicional: 0,
+        imagem: "Tomate.png"
     }
 
-
-
-
 ]
+
+export default ingredientes;
